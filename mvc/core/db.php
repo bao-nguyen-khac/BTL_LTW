d329d3248c0b1cca4a6977e832c721b59b430081
duongham2511 <duongham2511@gmail.com>
2021-11-23 14:08:13 +0700

    added login frontend

--- mvc/core/db.php
+++ mvc/core/db.php
@@ -5,7 +5,7 @@ class db{
     public $connect;
     protected $servername = "localhost";
     protected $username = "root";
-    protected $password = "bao7122001";
+    protected $password = "minhduong2000";
     protected $dbname = "ass_ltw";
 
     function __construct(){
